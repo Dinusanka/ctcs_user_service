3c5e7d2edfdeaf427f995ee9caa0d1af1a910ee5
IT22923288 <141344358+Dinusanka@users.noreply.github.com>
2026-05-02 00:16:15 +0530

    chore: add backend gitignore and update user-data startup script with deployment commands

--- user-data.sh
+++ user-data.sh
@@ -5,3 +5,6 @@ sudo apt install docker.io docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
+#ssh -i "user-service-key.pem" ubuntu@13.206.187.253
+#git commit --allow-empty -m "msg"
+#git push origin main
